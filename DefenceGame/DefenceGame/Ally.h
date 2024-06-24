a10534bfb30a7c7ea1112100fb653f723866277b
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-06-24 20:14:54 +0900

    fix : Ally::lastAttackTime Data Types, delete : Unused Variable

--- DefenceGame/DefenceGame/Ally.h
+++ DefenceGame/DefenceGame/Ally.h
@@ -8,11 +8,10 @@ public:
 	virtual ~Ally();
 protected:
 	int _attackTime = 500;
-	int _lastAttackTime = 0;
-	clock_t _attackTimer;
 	int _attackRange = 5;
 	int _damage = 5;
 	int _price = 20;
+	clock_t _lastAttackTime = 0;
 public:
 	int getAttackRange() { return _attackRange; }
 	void update() override;
