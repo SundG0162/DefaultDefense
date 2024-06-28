1d6349cbca79a9ba3b1234c86f220d532cf965c5
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-06-28 12:08:25 +0900

    remove : Unused part

--- DefenceGame/DefenceGame/InGameScene.h
+++ DefenceGame/DefenceGame/InGameScene.h
@@ -16,12 +16,9 @@ public:
 	void render() override;
 public:
 	void changeState(INGAMESCENE_STATE state);
-	void entityUpdate();
 	void mapRender();
 	void uiRender();
-	void selectUIRender();
 	void entityRender(const Vector2& pos);
-	KEY KeyController();
 private:
 	InGameState* _currentState;
 	std::map<INGAMESCENE_STATE, InGameState*> _stateMap;
