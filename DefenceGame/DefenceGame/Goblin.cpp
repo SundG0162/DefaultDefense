c72c9ecdbe0e500811b998f73a62edb6885ed8e2
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-06-30 22:41:32 +0900

    feat : Buff Enemy

--- DefenceGame/DefenceGame/Goblin.cpp
+++ DefenceGame/DefenceGame/Goblin.cpp
@@ -8,7 +8,7 @@ Goblin::Goblin(ENTITY_TYPE type, std::string renderString, COLOR color, int hp, 
 	_type = type;
 	_renderString = renderString;
 	_color = color;
-	_hp = std::round((float)hp * (float)(1 + GET_SINGLETON(WaveManager)->getCurrentWave() / 40.0f));
+	_hp = std::round((float)hp * (float)(1 + GET_SINGLETON(WaveManager)->getCurrentWave() / 10.0f));
 	_moveTime = moveTime;
 	_defaultMoveTime = moveTime;
 	_rewardGold = rewardGold;
